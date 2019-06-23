2bfb3303c800f9646b3b20ab576dc1e35b5cfe17
Faiq <faiq.kaboel@gmail.com>
2019-06-24 04:06:19 +0700

    Setup states and data fetcher from json-placeholder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Posts from "./_components/Posts";
+import AddPost from "./_components/AddPost";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(res => this.setState({ posts: res }))
+            .catch(err => {
+                alert('An Error Has Occured during data fetching');
+            })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="Logo"/>
+                </header>
+                <div className="container">
+                    <AddPost/>
+                    <Posts posts={this.state.posts}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
